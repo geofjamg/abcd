57fa87a53c3dae86cb09d1a35bbe858d93e06fc1
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2011-07-16 18:59:27 +0000

    Improve finally uninlining

--- src/main/java/fr/jamgotchian/abcd/core/analysis/FinallyUninliner.java
+++ src/main/java/fr/jamgotchian/abcd/core/analysis/FinallyUninliner.java
@@ -16,6 +16,7 @@
  */
 package fr.jamgotchian.abcd.core.analysis;
 
+import com.google.common.base.Objects;
 import fr.jamgotchian.abcd.core.common.ABCDException;
 import fr.jamgotchian.abcd.core.controlflow.BasicBlock;
 import fr.jamgotchian.abcd.core.controlflow.ControlFlowGraph;
@@ -52,7 +53,9 @@ import fr.jamgotchian.abcd.core.tac.model.TACInstVisitor;
 import fr.jamgotchian.abcd.core.tac.model.ThrowInst;
 import fr.jamgotchian.abcd.core.tac.model.UnaryInst;
 import fr.jamgotchian.abcd.core.tac.model.Variable;
+import fr.jamgotchian.abcd.core.tac.model.VariableID;
 import fr.jamgotchian.abcd.core.tac.util.TACInstWriter;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -73,120 +76,327 @@ public class FinallyUninliner {
         logger.setLevel(Level.FINEST);
     }
 
+    private static class VariableComparator {
+
+        /* Variable ID mapping between temporary variables of the 2 sequences */
+        private final Map<VariableID, VariableID> mapping
+                = new HashMap<VariableID, VariableID>();
+
+        private boolean defEqual(Variable var1, Variable var2) {
+            if (mapping.containsKey(var1.getID())
+                    || mapping.containsKey(var2.getID())) {
+                throw new IllegalStateException("Finally uninlining should be run on SSA form");
+            }
+            mapping.put(var1.getID(), var2.getID());
+            mapping.put(var2.getID(), var1.getID());
+            return true;
+        }
+
+        private boolean useEqual(Variable var1, Variable var2) {
+            if (var1.getID().equals(var2.getID())) {
+                return true;
+            }
+            boolean a =  var2.getID().equals(mapping.get(var1.getID()));
+            return a;
+        }
+
+        private boolean usesEqual(List<Variable> vars1, List<Variable> vars2) {
+            if (vars1.size() != vars2.size()) {
+                return false;
+            }
+            for (int i = 0; i < vars1.size(); i++) {
+                if (!useEqual(vars1.get(i), vars2.get(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private boolean usesEqual(Set<Variable> vars1, Set<Variable> vars2) {
+            if (vars1.size() != vars2.size()) {
+                return false;
+            }
+            for (Variable var1 : vars1) {
+                boolean found = false;
+                for (Variable var2 : vars2) {
+                    if (useEqual(var1, var2)) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
     private static class TACComparator implements TACInstVisitor<Boolean, TACInst> {
 
         private final TACInstSeq otherSeq;
 
-        private TACComparator(TACInstSeq otherSeq) {
+        private final VariableComparator comparator;
+
+        private TACComparator(TACInstSeq otherSeq, VariableComparator comparator) {
             this.otherSeq = otherSeq;
+            this.comparator = comparator;
         }
 
         public Boolean visit(TACInstSeq seq, TACInst arg) {
-            return null; // TODO
+            if (seq.size() != otherSeq.size()) {
+                return Boolean.FALSE;
+            }
+            for (int i = 0; i < seq.size(); i++) {
+                TACInst inst = seq.get(i);
+                TACInst inst2 = otherSeq.get(i);
+                if (Boolean.FALSE.equals(inst.accept(this, inst2))) {
+                    return Boolean.FALSE;
+                }
+            }
+            return Boolean.TRUE;
         }
 
         public Boolean visit(ArrayLengthInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof ArrayLengthInst)) {
+                return Boolean.FALSE;
+            }
+            ArrayLengthInst inst2 = (ArrayLengthInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && comparator.useEqual(inst.getArray(), inst2.getArray());
         }
 
         public Boolean visit(AssignConstInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof AssignConstInst)) {
+                return Boolean.FALSE;
+            }
+            AssignConstInst inst2 = (AssignConstInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && inst.getConst().equals(inst2.getConst());
         }
 
         public Boolean visit(AssignVarInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof AssignVarInst)) {
+                return Boolean.FALSE;
+            }
+            AssignVarInst inst2 = (AssignVarInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && comparator.useEqual(inst.getValue(), inst2.getValue());
         }
 
         public Boolean visit(BinaryInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof BinaryInst)) {
+                return Boolean.FALSE;
+            }
+            BinaryInst inst2 = (BinaryInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && comparator.useEqual(inst.getLeft(), inst2.getLeft())
+                    && comparator.useEqual(inst.getRight(), inst2.getRight())
+                    && inst.getOperator() == inst2.getOperator();
         }
 
         public Boolean visit(CallMethodInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof CallMethodInst)) {
+                return Boolean.FALSE;
+            }
+            CallMethodInst inst2 = (CallMethodInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && comparator.useEqual(inst.getObject(), inst2.getObject())
+                    && inst.getSignature().equals(inst2.getSignature())
+                    && comparator.usesEqual(inst.getArguments(), inst2.getArguments());
         }
 
         public Boolean visit(CallStaticMethodInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof CallStaticMethodInst)) {
+                return Boolean.FALSE;
+            }
+            CallStaticMethodInst inst2 = (CallStaticMethodInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && inst.getScope().equals(inst2.getScope())
+                    && inst.getSignature().equals(inst2.getSignature())
+                    && comparator.usesEqual(inst.getArguments(), inst2.getArguments());
         }
 
         public Boolean visit(CastInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof CastInst)) {
+                return Boolean.FALSE;
+            }
+            CastInst inst2 = (CastInst) arg;
+           return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && inst.getCastType().equals(inst2.getCastType())
+                    && comparator.useEqual(inst.getVar(), inst2.getVar());
         }
 
         public Boolean visit(ConditionalInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof ConditionalInst)) {
+                return Boolean.FALSE;
+            }
+            ConditionalInst inst2 = (ConditionalInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && comparator.useEqual(inst.getCond(), inst2.getCond())
+                    && comparator.useEqual(inst.getThen(), inst2.getThen())
+                    && comparator.useEqual(inst.getElse(), inst2.getElse());
         }
 
         public Boolean visit(GetArrayInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof GetArrayInst)) {
+                return Boolean.FALSE;
+            }
+            GetArrayInst inst2 = (GetArrayInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && comparator.useEqual(inst.getArray(), inst2.getArray())
+                    && comparator.useEqual(inst.getIndex(), inst2.getIndex());
         }
 
         public Boolean visit(SetArrayInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof SetArrayInst)) {
+                return Boolean.FALSE;
+            }
+            SetArrayInst inst2 = (SetArrayInst) arg;
+            return comparator.useEqual(inst.getArray(), inst2.getArray())
+                    && comparator.useEqual(inst.getIndex(), inst2.getIndex())
+                    && comparator.useEqual(inst.getValue(), inst2.getValue());
         }
 
         public Boolean visit(GetFieldInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof GetFieldInst)) {
+                return Boolean.FALSE;
+            }
+            GetFieldInst inst2 = (GetFieldInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && comparator.useEqual(inst.getObject(), inst2.getObject())
+                    && inst.getFieldName().equals(inst2.getFieldName());
         }
 
         public Boolean visit(SetFieldInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof SetFieldInst)) {
+                return Boolean.FALSE;
+            }
+            SetFieldInst inst2 = (SetFieldInst) arg;
+            return comparator.useEqual(inst.getObject(), inst2.getObject())
+                    && inst.getFieldName().equals(inst2.getFieldName())
+                    && comparator.defEqual(inst.getValue(), inst2.getValue());
         }
 
         public Boolean visit(JumpIfInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof JumpIfInst)) {
+                return Boolean.FALSE;
+            }
+            JumpIfInst inst2 = (JumpIfInst) arg;
+            return comparator.useEqual(inst.getCond(), inst2.getCond());
         }
 
         public Boolean visit(InstanceOfInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof InstanceOfInst)) {
+                return Boolean.FALSE;
+            }
+            InstanceOfInst inst2 = (InstanceOfInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && inst.getType().equals(inst2.getType())
+                    && comparator.useEqual(inst.getVar(), inst2.getVar());
         }
 
         public Boolean visit(MonitorEnterInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof MonitorEnterInst)) {
+                return Boolean.FALSE;
+            }
+            MonitorEnterInst inst2 = (MonitorEnterInst) arg;
+            return comparator.useEqual(inst.getObj(), inst2.getObj());
         }
 
         public Boolean visit(MonitorExitInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof MonitorExitInst)) {
+                return Boolean.FALSE;
+            }
+            MonitorExitInst inst2 = (MonitorExitInst) arg;
+            return comparator.useEqual(inst.getObj(), inst2.getObj());
         }
 
         public Boolean visit(NewArrayInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof NewArrayInst)) {
+                return Boolean.FALSE;
+            }
+            NewArrayInst inst2 = (NewArrayInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && inst.getType().equals(inst2.getType())
+                    && comparator.usesEqual(inst.getDimensions(), inst2.getDimensions());
         }
 
         public Boolean visit(NewObjectInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof NewObjectInst)) {
+                return Boolean.FALSE;
+            }
+            NewObjectInst inst2 = (NewObjectInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && inst.getType().equals(inst2.getType())
+                    && comparator.usesEqual(inst.getArguments(), inst2.getArguments());
         }
 
         public Boolean visit(ReturnInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof ReturnInst)) {
+                return Boolean.FALSE;
+            }
+            ReturnInst inst2 = (ReturnInst) arg;
+            return comparator.useEqual(inst.getVar(), inst2.getVar());
         }
 
         public Boolean visit(SwitchInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof SwitchInst)) {
+                return Boolean.FALSE;
+            }
+            SwitchInst inst2 = (SwitchInst) arg;
+            return comparator.useEqual(inst.getIndex(), inst2.getIndex());
         }
 
         public Boolean visit(ThrowInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof ThrowInst)) {
+                return Boolean.FALSE;
+            }
+            ThrowInst inst2 = (ThrowInst) arg;
+            return comparator.useEqual(inst.getVar(), inst2.getVar());
         }
 
         public Boolean visit(UnaryInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof UnaryInst)) {
+                return Boolean.FALSE;
+            }
+            UnaryInst inst2 = (UnaryInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && inst.getOperator() == inst2.getOperator()
+                    && comparator.useEqual(inst.getVar(), inst2.getVar());
         }
 
         public Boolean visit(ChoiceInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof ChoiceInst)) {
+                return Boolean.FALSE;
+            }
+            ChoiceInst inst2 = (ChoiceInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && comparator.usesEqual(inst.getChoices(), inst2.getChoices());
         }
 
         public Boolean visit(PhiInst inst, TACInst arg) {
-            return null; // TODO
+            throw new ABCDException("Should not have Phi instruction during finally uninlining");
         }
 
         public Boolean visit(GetStaticFieldInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof GetStaticFieldInst)) {
+                return Boolean.FALSE;
+            }
+            GetStaticFieldInst inst2 = (GetStaticFieldInst) arg;
+            return comparator.defEqual(inst.getResult(), inst2.getResult())
+                    && inst.getScope().equals(inst2.getScope())
+                    && inst.getFieldName().equals(inst2.getFieldName());
         }
 
         public Boolean visit(SetStaticFieldInst inst, TACInst arg) {
-            return null; // TODO
+            if (!(arg instanceof SetStaticFieldInst)) {
+                return Boolean.FALSE;
+            }
+            SetStaticFieldInst inst2 = (SetStaticFieldInst) arg;
+            return inst.getScope().equals(inst2.getScope())
+                    && inst.getFieldName().equals(inst2.getFieldName())
+                    && comparator.useEqual(inst.getValue(), inst2.getValue());
         }
     }
 
@@ -257,23 +467,69 @@ public class FinallyUninliner {
                 }
             }
 
-            logger.log(Level.FINEST, "Exit BB for finally with {0} header : {1}",
+            logger.log(Level.FINEST, "Try exits for finally subgraph (entry={0}) : {1}",
                     new Object[] {finallyEntry, tryExits});
 
             // uninline finally subgraph at try exits
+            for (BasicBlock tryExit : tryExits) {
+                Set<BasicBlock> toRemove = new HashSet<BasicBlock>();
+                VariableComparator comparator = new VariableComparator();
+                if (subgraphEquals(tryExit, finallyEntry, finallySubgraph,
+                                   toRemove, comparator)) {
+                    logger.log(Level.FINEST, "Remove finally subgraph : {0}",
+                            toRemove);
 
-            // TODO
+//                        for (BasicBlock bb : toRemove) {
+//                            CFG.removeBasicBlock(bb);
+//                        }
+                } else {
+                    throw new ABCDException("Unable to uninline finally subgraph (entry=" +
+                            finallyEntry + ") at try exit " + tryExit);
+                }
+            }
         }
     }
 
-    private boolean subgraphEquals(BasicBlock bb1, BasicBlock bb2) {
-        TACInstSeq seq1 = ((AnalysisData) bb1.getData()).getInstructions();
-        TACInstSeq seq2 = ((AnalysisData) bb2.getData()).getInstructions();
-        Boolean equals = seq1.accept(new TACComparator(seq2), null);
+    private boolean subgraphEquals(BasicBlock block, BasicBlock otherBlock,
+                                   DirectedGraph<BasicBlock, Edge> otherSubgraph,
+                                   Set<BasicBlock> subgraph, VariableComparator comparator) {
+        TACInstSeq seq1 = ((AnalysisData) block.getData()).getInstructions();
+        TACInstSeq seq2 = ((AnalysisData) otherBlock.getData()).getInstructions();
+        Boolean equals = seq1.accept(new TACComparator(seq2, comparator), null);
         if (Boolean.FALSE.equals(equals)) {
             return false;
         }
-        // TODO
+
+        subgraph.add(block);
+
+        if (otherSubgraph.getSuccessorCountOf(otherBlock) == 0) {
+            return true;
+        }
+
+        if (CFG.getSuccessorCountOf(block)
+                != otherSubgraph.getSuccessorCountOf(otherBlock)) {
+            return false;
+        }
+
+        for (Edge outgoingEdge : CFG.getOutgoingEdgesOf(block)) {
+            BasicBlock successor = CFG.getEdgeTarget(outgoingEdge);
+            boolean match = false;
+            for (Edge otherOutgoingEdge : otherSubgraph.getOutgoingEdgesOf(otherBlock)) {
+                if (Objects.equal(outgoingEdge.getValue(), otherOutgoingEdge.getValue())
+                        && outgoingEdge.isLoopExit() == otherOutgoingEdge.isLoopExit()) {
+                    BasicBlock otherSuccessor = otherSubgraph.getEdgeTarget(otherOutgoingEdge);
+                    if (subgraphEquals(successor, otherSuccessor, otherSubgraph,
+                                       subgraph, comparator)) {
+                        match = true;
+                        break;
+                    }
+                }
+            }
+            if (!match) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
