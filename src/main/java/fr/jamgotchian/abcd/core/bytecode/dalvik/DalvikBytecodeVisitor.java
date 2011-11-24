03d41b931912b9f6c26bcde2c941de3a02043ad4
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2011-11-24 13:04:22 +0100

    Dalvik bytecode writing

--- src/main/java/fr/jamgotchian/abcd/core/bytecode/dalvik/DalvikBytecodeVisitor.java
+++ src/main/java/fr/jamgotchian/abcd/core/bytecode/dalvik/DalvikBytecodeVisitor.java
@@ -21,10 +21,22 @@ import fr.jamgotchian.abcd.core.util.Range;
 import org.jf.dexlib.Code.Format.Instruction10t;
 import org.jf.dexlib.Code.Format.Instruction10x;
 import org.jf.dexlib.Code.Format.Instruction11n;
+import org.jf.dexlib.Code.Format.Instruction11x;
+import org.jf.dexlib.Code.Format.Instruction12x;
 import org.jf.dexlib.Code.Format.Instruction21c;
+import org.jf.dexlib.Code.Format.Instruction21h;
+import org.jf.dexlib.Code.Format.Instruction21s;
 import org.jf.dexlib.Code.Format.Instruction21t;
+import org.jf.dexlib.Code.Format.Instruction22b;
+import org.jf.dexlib.Code.Format.Instruction22c;
 import org.jf.dexlib.Code.Format.Instruction22t;
+import org.jf.dexlib.Code.Format.Instruction23x;
+import org.jf.dexlib.Code.Format.Instruction30t;
+import org.jf.dexlib.Code.Format.Instruction31i;
+import org.jf.dexlib.Code.Format.Instruction31t;
 import org.jf.dexlib.Code.Format.Instruction35c;
+import org.jf.dexlib.Code.Format.Instruction51l;
+import org.jf.dexlib.Code.Format.PackedSwitchDataPseudoInstruction;
 import org.jf.dexlib.Code.Instruction;
 
 /**
@@ -37,17 +49,41 @@ public abstract class DalvikBytecodeVisitor {
 
     public abstract void visit(BasicBlock bb, int position, Instruction10t inst, CodeAddressManager addressManager);
 
+    public abstract void visit(BasicBlock bb, int position, Instruction10x inst);
+
     public abstract void visit(BasicBlock bb, int position, Instruction11n inst);
+
+    public abstract void visit(BasicBlock bb, int position, Instruction11x inst);
+
+    public abstract void visit(BasicBlock bb, int position, Instruction12x inst);
 
     public abstract void visit(BasicBlock bb, int position, Instruction21c inst);
 
+    public abstract void visit(BasicBlock bb, int position, Instruction21h inst);
+
+    public abstract void visit(BasicBlock bb, int position, Instruction21s inst);
+
     public abstract void visit(BasicBlock bb, int position, Instruction21t inst, CodeAddressManager addressManager);
+
+    public abstract void visit(BasicBlock bb, int position, Instruction22b inst);
+
+    public abstract void visit(BasicBlock bb, int position, Instruction22c inst);
 
     public abstract void visit(BasicBlock bb, int position, Instruction22t inst, CodeAddressManager addressManager);
 
+    public abstract void visit(BasicBlock bb, int position, Instruction23x inst);
+
+    public abstract void visit(BasicBlock bb, int position, Instruction30t inst, CodeAddressManager addressManager);
+
+    public abstract void visit(BasicBlock bb, int position, Instruction31i inst);
+
+    public abstract void visit(BasicBlock bb, int position, Instruction31t inst, CodeAddressManager addressManager);
+
     public abstract void visit(BasicBlock bb, int position, Instruction35c inst);
 
-    public abstract void visit(BasicBlock bb, int position, Instruction10x inst);
+    public abstract void visit(BasicBlock bb, int position, Instruction51l inst);
+
+    public abstract void visit(BasicBlock bb, int position, PackedSwitchDataPseudoInstruction inst, CodeAddressManager addressManager);
 
     public abstract void visitTODO(BasicBlock bb, int position, Instruction inst);
 
@@ -74,20 +110,68 @@ public abstract class DalvikBytecodeVisitor {
                         visit(bb, position, (Instruction11n) inst);
                         break;
 
+                    case Format11x:
+                        visit(bb, position, (Instruction11x) inst);
+                        break;
+
+                    case Format12x:
+                        visit(bb, position, (Instruction12x) inst);
+                        break;
+
                     case Format21c:
                         visit(bb, position, (Instruction21c) inst);
+                        break;
+
+                    case Format21h:
+                        visit(bb, position, (Instruction21h) inst);
+                        break;
+
+                    case Format21s:
+                        visit(bb, position, (Instruction21s) inst);
                         break;
 
                     case Format21t:
                         visit(bb, position, (Instruction21t) inst, addressManager);
                         break;
 
+                    case Format22b:
+                        visit(bb, position, (Instruction22b) inst);
+                        break;
+
+                    case Format22c:
+                        visit(bb, position, (Instruction22c) inst);
+                        break;
+
                     case Format22t:
                         visit(bb, position, (Instruction22t) inst, addressManager);
                         break;
 
+                    case Format23x:
+                        visit(bb, position, (Instruction23x) inst);
+                        break;
+
+                    case Format30t:
+                        visit(bb, position, (Instruction30t) inst, addressManager);
+                        break;
+
+                    case Format31i:
+                        visit(bb, position, (Instruction31i) inst);
+                        break;
+
+                    case Format31t:
+                        visit(bb, position, (Instruction31t) inst, addressManager);
+                        break;
+
                     case Format35c:
                         visit(bb, position, (Instruction35c) inst);
+                        break;
+
+                    case Format51l:
+                        visit(bb, position, (Instruction51l) inst);
+                        break;
+
+                    case PackedSwitchData:
+                        visit(bb, position, (PackedSwitchDataPseudoInstruction) inst, addressManager);
                         break;
 
                     default:
--- src/main/java/fr/jamgotchian/abcd/core/bytecode/dalvik/DalvikBytecodeWriter.java
+++ src/main/java/fr/jamgotchian/abcd/core/bytecode/dalvik/DalvikBytecodeWriter.java
@@ -24,16 +24,33 @@ import fr.jamgotchian.abcd.core.ir.BasicBlockImpl;
 import fr.jamgotchian.abcd.core.ir.BasicBlockType;
 import java.io.StringWriter;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import org.jf.dexlib.Code.Format.Instruction10t;
 import org.jf.dexlib.Code.Format.Instruction10x;
 import org.jf.dexlib.Code.Format.Instruction11n;
+import org.jf.dexlib.Code.Format.Instruction11x;
+import org.jf.dexlib.Code.Format.Instruction12x;
 import org.jf.dexlib.Code.Format.Instruction21c;
+import org.jf.dexlib.Code.Format.Instruction21h;
+import org.jf.dexlib.Code.Format.Instruction21s;
 import org.jf.dexlib.Code.Format.Instruction21t;
+import org.jf.dexlib.Code.Format.Instruction22b;
+import org.jf.dexlib.Code.Format.Instruction22c;
 import org.jf.dexlib.Code.Format.Instruction22t;
+import org.jf.dexlib.Code.Format.Instruction23x;
+import org.jf.dexlib.Code.Format.Instruction30t;
+import org.jf.dexlib.Code.Format.Instruction31i;
+import org.jf.dexlib.Code.Format.Instruction31t;
 import org.jf.dexlib.Code.Format.Instruction35c;
+import org.jf.dexlib.Code.Format.Instruction51l;
+import org.jf.dexlib.Code.Format.PackedSwitchDataPseudoInstruction;
+import org.jf.dexlib.Code.Format.PackedSwitchDataPseudoInstruction.PackedSwitchTarget;
 import org.jf.dexlib.Code.Instruction;
+import org.jf.dexlib.Item;
 import org.jf.dexlib.MethodIdItem;
+import org.jf.dexlib.StringIdItem;
+import org.jf.dexlib.TypeIdItem;
 
 /**
  *
@@ -59,6 +76,20 @@ public class DalvikBytecodeWriter extends DalvikBytecodeVisitor {
 
     public DalvikBytecodeWriter(CodeWriter writer) {
         this.writer = writer;
+    }
+
+    private void writeItem(Item<?> item) {
+        String itemStr = null;
+        if (item instanceof MethodIdItem) {
+            itemStr = ((MethodIdItem) item).getMethodString().toString();
+        } else if (item instanceof TypeIdItem) {
+            itemStr = ((TypeIdItem) item).getTypeDescriptor();
+        } else if (item instanceof StringIdItem) {
+            itemStr = ((StringIdItem) item).getStringValue();
+        } else {
+            itemStr = item.toString();
+        }
+        writer.write(writer.removeSpecialCharacters(itemStr));
     }
 
     @Override
@@ -94,7 +125,24 @@ public class DalvikBytecodeWriter extends DalvikBytecodeVisitor {
         writer.writeIndex(position)
                 .writeKeyword(inst.opcode.toString().toLowerCase())
                 .writeSpace().write("r").write(inst.getRegisterA())
-                .writeSpace().write(inst.getLiteral())
+                .writeSpace().write(Long.toString(inst.getLiteral()))
+                .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction11x inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA())
+                .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction12x inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA())
+                .writeSpace().write("r").write(inst.getRegisterB())
                 .newLine();
     }
 
@@ -102,8 +150,21 @@ public class DalvikBytecodeWriter extends DalvikBytecodeVisitor {
     public void visit(BasicBlock bb, int position, Instruction21c inst) {
         writer.writeIndex(position)
                 .writeKeyword(inst.opcode.toString().toLowerCase())
-                .writeSpace().write("r").write(inst.getRegisterA())
-                .writeSpace().write(inst.getReferencedItem())
+                .writeSpace()
+                .write("r").write(inst.getRegisterA())
+                .writeSpace();
+        writeItem(inst.getReferencedItem());
+        writer.newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction21h inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace()
+                .write("r").write(inst.getRegisterA())
+                .writeSpace()
+                .write(Long.toString(inst.getLiteral()))
                 .newLine();
     }
 
@@ -121,6 +182,35 @@ public class DalvikBytecodeWriter extends DalvikBytecodeVisitor {
     }
 
     @Override
+    public void visit(BasicBlock bb, int position, Instruction21s inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA())
+                .writeSpace().write(Long.toString(inst.getLiteral()))
+                .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction22b inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA())
+                .writeSpace().write("r").write(inst.getRegisterB())
+                .writeSpace().write(Long.toString(inst.getLiteral()))
+                .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction22c inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA())
+                .writeSpace().write("r").write(inst.getRegisterB()).writeSpace();
+        writeItem(inst.getReferencedItem());
+        writer.newLine();
+    }
+
+    @Override
     public void visit(BasicBlock bb, int position, Instruction22t inst,
                       CodeAddressManager addressManager) {
         int targetPosition
@@ -135,17 +225,87 @@ public class DalvikBytecodeWriter extends DalvikBytecodeVisitor {
     }
 
     @Override
+    public void visit(BasicBlock bb, int position, Instruction23x inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA())
+                .writeSpace().write("r").write(inst.getRegisterB())
+                .writeSpace().write("r").write(inst.getRegisterC())
+                .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction30t inst, CodeAddressManager addressManager) {
+        int targetPosition
+                = addressManager.getTargetPosition(position,
+                                                   inst.getTargetAddressOffset());
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write(targetPosition)
+                .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction31i inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA()).writeSpace()
+                .write(Long.toString(inst.getLiteral()))
+                .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction31t inst,
+                      CodeAddressManager addressManager) {
+        int targetPosition
+                = addressManager.getTargetPosition(position,
+                                                   inst.getTargetAddressOffset());
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA()).writeSpace()
+                .write(targetPosition)
+                .newLine();
+    }
+
+    @Override
     public void visit(BasicBlock bb, int position, Instruction35c inst) {
         writer.writeIndex(position)
-                .writeKeyword(inst.opcode.toString().toLowerCase());
-        MethodIdItem methodId = (MethodIdItem) inst.getReferencedItem();
-        writer.writeSpace().write(writer.removeSpecialCharacters(methodId.getMethodString().toString()));
+                .writeKeyword(inst.opcode.toString().toLowerCase()).writeSpace();
+        writeItem(inst.getReferencedItem());
         List<String> registers
                 = Arrays.asList("r" + inst.getRegisterA(), "r" + inst.getRegisterD(),
                                 "r" + inst.getRegisterE(), "r" + inst.getRegisterF(),
                                 "r" + inst.getRegisterG());
         writer.writeSpace().write(registers.subList(0, inst.getRegCount()))
                 .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, Instruction51l inst) {
+        writer.writeIndex(position)
+                .writeKeyword(inst.opcode.toString().toLowerCase())
+                .writeSpace().write("r").write(inst.getRegisterA()).writeSpace()
+                .write(Long.toString(inst.getLiteral()))
+                .newLine();
+    }
+
+    @Override
+    public void visit(BasicBlock bb, int position, PackedSwitchDataPseudoInstruction inst,
+                      CodeAddressManager addressManager) {
+        writer.incrIndent();
+        for (Iterator<PackedSwitchTarget> it = inst.iterateKeysAndTargets(); it.hasNext();) {
+            PackedSwitchTarget target = it.next();
+            writer.writeIndex(-1); // just for alignment
+            writer.write(Integer.toString(target.value));
+            writer.write(':');
+            writer.writeSpace();
+//            int targetPosition
+//                    = addressManager.getTargetPosition(position,
+//                                                       target.targetAddressOffset);
+            writer.write(target.targetAddressOffset);
+            writer.newLine();
+        }
+        writer.decrIndent();
     }
 
     @Override
